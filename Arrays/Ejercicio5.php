b3969998ecd3535527caf42ea252cfeaa486d2f5
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2025-10-29 21:53:25 +0100

    ok

--- Arrays/Ejercicio5.php
+++ Arrays/Ejercicio5.php
@@ -4,7 +4,7 @@ Genera un array aleatorio de 33 elementos con numeros comprendidos entre 0 y 100
 y calcula el mayor el menor y la media 
 
 */
-$mayor=0;
+$mayor=0; 
 $menor=100;
 $suma=0;
 for ($i=0; $i <33 ; $i++) { 
---
+++ Arrays/Ejercicio6.php
@@ -0,0 +1,12 @@
+<?php
+//Rellenar un array de 100 elemenos de manera aleatorioa con los valores "m" o "f".
+//Unja vez completado calcula cuantos hay de cada uno almacenando cada recuento en 
+// unh array asociativo 
+
+$array=[]
+
+for ($i=0;$i<100;$i++){
+    $array[]=rand
+}
+
+?>
