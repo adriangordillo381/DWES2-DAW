633e634232b7a7b9a3e62d1e58336f6cb23d0020
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2025-10-29 21:53:00 +0100

    ok

--- Arrays/brisca.php
+++ Arrays/brisca.php
@@ -1,4 +1,5 @@
-<?php
+3
+.<?php
 /*
 Realiza u programa que escoja al azar3  10 cartas de la baraja española
 y que diga cuantos puntos suman segun el juego de la brisca
@@ -7,7 +8,6 @@ segun el nombre de la carta
 */
 
 
-// Array con los puntos de cada carta según la Brisca
 $puntos = [
     1 => 11,
     2 => 0,
@@ -21,13 +21,8 @@ $puntos = [
     12 => 4
 ];
 
-// Palos de la baraja española
-$palos = ["oros", "copas", "espadas", "bastos"];
 
-
-
-//Creamos la baraja
-
+$palos = ["oro", "copas", "espada", "bastos"];
 
 
 
@@ -35,14 +30,15 @@ $baraja=[];
 for ($i=0; $i<count($palos); $i++) { 
     for($j=1;$j<=12;$j++){
         if($j!==8 && $j!==9){
-         $baraja[]=["numero" =>$j,"palos" =>$palos[$i]];   
+         $baraja[]=["numero" =>$j,
+                    "palos" =>$palos[$i]];   
         }
     }
 }
-//Mezclamos la baraja
+
 shuffle($baraja);
 
-//Escogemos las diez cartas
+
 $mano=[];
 for ($i=1; $i <=10 ; $i++) { 
     $mano[]=$baraja[$i];
@@ -60,8 +56,11 @@ for ($i=0; $i <count($mano) ; $i++) {
 echo "Mano:<br>";
 foreach($mano as $carta){
    $numero=$carta["numero"]; 
+   
+echo '<img src="img/' . $carta['numero'] . $carta['palos'] . '.png">';
 echo "{$carta['numero']} de {$carta['palos']} que tienen {$puntos[$numero]} puntos<br>";
 echo "<br>";
+
 }
 
 
